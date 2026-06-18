1275e825e28a996137406b40ca7e946bee48b50c
peterjoshuapjg <peterjoshua.pjg@gmail.com>
2026-06-18 10:57:19 +0800

    Added calculator operations

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 
 class Program
 {
@@ -11,9 +10,44 @@ class Program
             string input = Console.ReadLine();
 
             if (input.ToLower() == "exit")
-            {
                 break;
+
+            double num1 = Convert.ToDouble(input);
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operator (+, -, *, /): ");
+            string op = Console.ReadLine();
+
+            double result = 0;
+
+            if (op == "+")
+                result = num1 + num2;
+            else if (op == "-")
+                result = num1 - num2;
+            else if (op == "*")
+                result = num1 * num2;
+            else if (op == "/")
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero.");
+                    continue;
+                }
+
+                result = num1 / num2;
             }
+            else
+            {
+                Console.WriteLine("Invalid operator.");
+                continue;
+            }
+
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine();
         }
+
+        Console.WriteLine("Program ends.");
     }
 }
