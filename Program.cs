dd40adf6836f749774a20a9fbf3ee4ac74180013
peterjoshuapjg <peterjoshua.pjg@gmail.com>
2026-06-18 10:54:53 +0800

    Implemented calculator input loop

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Calculator Program");
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Enter first number (or 'exit' to quit): ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "exit")
+            {
+                break;
+            }
+        }
+    }
+}
